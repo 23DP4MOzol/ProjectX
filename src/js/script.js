7826335cf69846caef52000721ee0706b657a890
Maris Ozols <A230323MO@rvt.lv>
2025-03-20 11:22:52 +0200

    Majaslapas izveide

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for the sidebar links
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
